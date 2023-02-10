8b3c8ee2412235695c805553134dc94c25e19e83
Gunes Akgun <mgunesak@gmail.com>
2023-02-11 02:05:35 +0300

    ex-9 chain

--- ex-9.js
+++ ex-9.js
@@ -1,4 +1,33 @@
-const people = ["Joe", "Caroline", "Sabrina"];
+// const people = ["Joe", "Caroline", "Sabrina"];
+
+// function luckyDraw(player) {
+//   return new Promise((resolve, reject) => {
+//     const win = Boolean(Math.round(Math.random()));
+
+//     process.nextTick(() => {
+//       if (win) {
+//         resolve(`${player} won a prize in the draw!`);
+//       } else {
+//         reject(new Error(`${player} lost the draw.`));
+//       }
+//     });
+//   });
+// }
+
+// const iterableFunc = async (loopIn, action) => {
+//   for (const el of loopIn) {
+//     try {
+//       const result = await action(el);
+//       console.log(result);
+//     } catch (err) {
+//       console.log(`${err}`);
+//     }
+//   }
+// };
+
+// iterableFunc(people, luckyDraw);
+
+const players = ["Joe", "Caroline", "Sabrina"];
 
 function luckyDraw(player) {
   return new Promise((resolve, reject) => {
@@ -14,15 +43,14 @@ function luckyDraw(player) {
   });
 }
 
-const iterableFunc = async (loopIn, action) => {
-  for (const el of loopIn) {
-    try {
-      const result = await action(el);
-      console.log(result);
-    } catch (err) {
-      console.log(`${err}`);
-    }
-  }
-};
-
-iterableFunc(people, luckyDraw);
+players.reduce((promiseChain, player) => {
+  return promiseChain.then(() => {
+    return luckyDraw(player)
+      .then((resolvedValue) => {
+        console.log(resolvedValue);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  });
+}, Promise.resolve());
